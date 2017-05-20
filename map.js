63fa1feaec6be907f9a5a71b50e8fc61fa3d8006
Tom Reilly <treilly94@live.com>
2017-05-20 20:42:43 +0100

    Improved country input system

--- map.js
+++ map.js
@@ -1,23 +1,51 @@
 var map;
-var countries = ["'US'", "'GB'", "'DE'"];
-var country_str = "ISO_2DIGIT IN (" + countries + ")"
+var countryList = ["'US'"];
+
+function updateCountryList() {
+    document.getElementById('countrys').innerHTML = ""
+    for (i = 0; i < countryList.length; i++) {
+        var newCountry = countryList[i] + " <br>";
+        document.getElementById('countrys').innerHTML += newCountry;
+    };
+}
+
+function addToCountryList() {
+    var addCountry = document.getElementById('addCountry').value;
+    addCountry = "'" + addCountry + "'"
+    if (addCountry.length == 2) {
+        alert("There is no value");
+    }
+    else if (countryList.includes(addCountry)) {
+        alert("This country is already in the list");
+    }
+    else {
+        countryList.push(addCountry);
+    }
+
+}
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 2,
-    center: {
-      lat: 41.948766,
-      lng: -87.691497
-    },
-  });
+    var country_str = "ISO_2DIGIT IN (" + countryList + ")"
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 2,
+        center: {
+            lat: 0.0,
+            lng: 0.0
+        },
+    });
 
-  var world_geometry = new google.maps.FusionTablesLayer({
+    var world_geometry = new google.maps.FusionTablesLayer({
     query: {
-      select: 'geometry',
-      from: '1N2LBk4JHwWpOY4d9fobIn27lfnZ5MDy-NoqqRpk',
-      where: country_str
-    },
-    map: map,
-    suppressInfoWindows: true
-  });
+            select: 'geometry',
+            from: '1N2LBk4JHwWpOY4d9fobIn27lfnZ5MDy-NoqqRpk',
+            where: country_str
+        },
+        map: map,
+        suppressInfoWindows: true
+    });
+}
+
+function refreshMap() {
+    //Doesnt work yet
+    google.maps.event.trigger(map, 'resize');
 }
