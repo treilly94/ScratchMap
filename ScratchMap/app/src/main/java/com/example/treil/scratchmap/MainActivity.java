19a0e794b7cbd06846d6851381b280ad2ca78ff5
Tom Reilly <treilly94@live.com>
2017-09-01 17:59:18 +0100

    added button and added to gradle

--- ScratchMap/app/src/main/java/com/example/treil/scratchmap/MainActivity.java
+++ ScratchMap/app/src/main/java/com/example/treil/scratchmap/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.treil.scratchmap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        // Respond to the menu button
+        Intent intent = new Intent(this, DisplayMessaageActivity.class);
+
     }
 }
