5b9b819aaff713f0a618d7732e4a15afb83ab42f
Tom Reilly <treilly94@live.com>
2017-09-01 19:05:53 +0100

    added step 2
    https://www.mapbox.com/android-docs/map-sdk/overview/

--- ScratchMap/app/src/main/java/com/example/treil/scratchmap/MainActivity.java
+++ ScratchMap/app/src/main/java/com/example/treil/scratchmap/MainActivity.java
@@ -5,17 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.mapbox.mapboxsdk.Mapbox;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Mapbox Access token
+        Mapbox.getInstance(getApplicationContext(), "mapbox_token");
     }
 
     public void sendMessage(View view) {
         // Respond to the menu button
-        Intent intent = new Intent(this, DisplayMessaageActivity.class);
 
+
+        //https://www.mapbox.com/android-docs/map-sdk/overview/
+        //Step 2
     }
 }
